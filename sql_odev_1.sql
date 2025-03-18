b3c5a2b24b422edf4b3e232f01dff6bdbcc5cee8
Tuğba Kaya <166510529+ttugbaa@users.noreply.github.com>
2025-03-18 11:15:14 +0300

    sql_odev_1.sql

--- sql_odev_1.sql
+++ sql_odev_1.sql
@@ -17,6 +17,6 @@ SELECT last_name
 FROM customer
 WHERE first_name = 'Mary';
 
--- film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+-- film tablosundaki uzunluğu(length) 50 den büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 SELECT * FROM film
 WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
